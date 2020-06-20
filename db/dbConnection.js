95868c73d150ae6c3e50b89ced047b810c1d04bf
Elias Roussos <eliasIsaiah@outlook.com>
2020-06-20 14:33:27 -0400

    added JAWSdb .env checker

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,19 +1,23 @@
 const mysql = require('mysql')
-
-const connection = mysql.createConnection({
+const connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWS_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PW,
     database: "sandbox_db"
   });
+}
 
-  console.log("user", process.env.DB_USER);
-  console.log("password", process.env.DB_PW);
-  
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("db connected")
-  });
-  
-  module.exports = connection;
+console.log("user", process.env.DB_USER);
+console.log("password", process.env.DB_PW);
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("db connected")
+});
+
+module.exports = connection;
